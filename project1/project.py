d48603a17d8e9ba1242394828f4db9ada8c1411d
Stéphane Champailler <schampailler@skynet.be>
2020-09-30 13:01:07 +0200

    fixed calls to make_data

--- project1/project.py
+++ project1/project.py
@@ -9,21 +9,15 @@ from data.plot import plot_boundary
 LEARNING_SET_SIZE=250
 TEST_SET_SIZE=10000
 
-LS_DATA = [ make_data1( LEARNING_SET_SIZE, random_state=1000),
-            make_data2( LEARNING_SET_SIZE, random_state=1000) ]
 
-TEST_DATA = [ make_data1( TEST_SET_SIZE, random_state=9999),
-              make_data2( TEST_SET_SIZE, random_state=9999) ]
+FUNCS = [make_data1, make_data2]
 
+for prob_ndx in range( len( FUNCS)):
 
-for prob_ndx in range( len( LS_DATA)):
-
-    learning_set = LS_DATA[prob_ndx]
     p_ndx = prob_ndx + 1
 
     print(f"Generating problem {p_ndx}")
-    inputs_ls = learning_set[0]
-    outputs_ls = learning_set[1]
+    inputs_ls, outputs_ls, inputs_ts, outputs_ts = FUNCS[prob_ndx](TEST_SET_SIZE, LEARNING_SET_SIZE, random_state=1000)
 
     DEPTHS = [1,2,4,8,None]
 
@@ -37,14 +31,12 @@ for prob_ndx in range( len( LS_DATA)):
         # plot_tree(clf, filled=True)
         # plt.show()
 
-        test_set = TEST_DATA[prob_ndx]
-        inputs_ts = test_set[0]
-        outputs_ts = test_set[1]
         predictions = clf.predict( inputs_ts)
 
-        ts_success_rate = 100.0 * np.sum( outputs_ts == predictions) / len(outputs_ts)
+        ts_error_rate = 100.0 * np.sum( outputs_ts != predictions) / len(outputs_ts)
 
         predictions_ls = clf.predict( inputs_ls)
-        ls_success_rate = 100.0 * np.sum( outputs_ls == predictions_ls) / len(outputs_ls)
+        ls_error_rate = 100.0 * np.sum( outputs_ls != predictions_ls) / len(outputs_ls)
 
-        print( "{} \t Success rate LS : {}%\t Success rate TS : {}%".format( f"Problem {p_ndx}, depth {depth}", ls_success_rate, ts_success_rate ))
+
+        print( "{} \t Error rate LS : {:.2f}%\t TS : {:.2f}%".format( f"Problem {p_ndx}, depth {depth}", ls_error_rate, ts_error_rate ))

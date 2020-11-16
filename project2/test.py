fixed variance computation

project2/test.py
@@ -135,21 +135,20 @@ def squared_bias_of_models_at_x(learning_algorithm, models, x, bayes_value):
 def variance_of_models_at_x(learning_algorithm, models, x, bayes_value):
     powers_of_x = np.array([ [x**i for i in range(0, learning_algorithm+1)] ])
 
+    # Ask each model to predict its value of y at point x
     predictions = []
     for model in models:
         predictions.append(model.predict(powers_of_x))
-    avg_pred = np.average(predictions)
+    predictions = np.array(predictions)
 
-    diff = []
-    for model in models:
-        diff = (model.predict(powers_of_x) - avg_pred)**2
-
-    return np.average(np.array(diff))
+    # variance is squared standard dev.
+    return np.average((predictions - np.average(predictions))**2)
 
 
 biases = []
 variances = []
-NB_LEARNING_SETS = 200 # seem stable 200
+NB_LEARNING_SETS = 400 # seem stable 200
+X_RANGE = np.linspace(0,2,100)
 
 for learning_algorithm in range(0, 5+1):
     print(f"training for algo m={learning_algorithm}")
@@ -157,7 +156,7 @@ for learning_algorithm in range(0, 5+1):
     models = train_models(learning_sets, learning_algorithm)
     bias = []
     variance = []
-    for x0 in np.arange(0, 2, 0.01):
+    for x0 in X_RANGE:
         bias.append(squared_bias_of_models_at_x(
             learning_algorithm, models, x0, bayes(x0)))
         variance.append(variance_of_models_at_x(
@@ -170,7 +169,7 @@ for learning_algorithm in range(0, 5+1):
 
 plt.figure(20)
 for algo, bias in enumerate(biases):
-    plt.scatter(np.arange(0, 2, 0.01), bias, marker='.', linewidths=0, label=f"m={algo}")
+    plt.scatter(X_RANGE, bias, marker='.', linewidths=0, label=f"m={algo}")
 
 for h in [0, 0.5, 1, 1.75]:
     plt.axvline(x=h,c='black')
@@ -184,7 +183,7 @@ plt.savefig("bias_d.pdf")
 
 plt.figure(21)
 for algo, variance in enumerate(variances):
-    plt.scatter(np.arange(0, 2, 0.01), variance, marker='.', linewidths=0, label=f"m={algo}")
+    plt.scatter(X_RANGE, variance, marker='.', linewidths=0, label=f"m={algo}")
 
 for h in [0, 0.5, 1, 1.75]:
     plt.axvline(x=h,c='black')
@@ -210,75 +209,76 @@ plt.legend()
 plt.savefig("q2d_bias_variance.pdf")
 
 
+######################################################################
 # # Start over with ridge regression
 
+def train_models_ridge(learning_sets, learning_algorithm, lambda_):
+    # Use the algorithm number learning_algorithm to train models over
+    # the learning set.
+    # We create as many models as learning sets.
 
-# def train_models_ridge(learning_sets, learning_algorithm, lambda_):
-#     # Use the algorithm number learning_algorithm to train models over
-#     # the learning set.
-#     # We create as many models as learning sets.
+    # The learning algorithm is a number in [0-5], that's
+    # the m number in the problem statement.
 
-#     # The learning algorithm is a number in [0-5], that's
-#     # the m number in the problem statement.
+    assert 0 <= learning_algorithm <= 5
 
-#     assert 0 <= learning_algorithm <= 5
+    models = []
+    for ys, xs in learning_sets:
 
-#     models = []
-#     for ys, xs in learning_sets:
+        # A model is a0 + a1 * x + a2 * x² + ...
+        # range will go from 0 to m inclusive !
 
-#         # A model is a0 + a1 * x + a2 * x² + ...
-#         # range will go from 0 to m inclusive !
+        # For the training, we use a0 + a1 * x_i + a2 * x_i² + ...
+        powers_of_x = np.stack([xs**i for i in range(0, learning_algorithm+1)])
 
-#         # For the training, we use a0 + a1 * x_i + a2 * x_i² + ...
-#         powers_of_x = np.stack([xs**i for i in range(0, learning_algorithm+1)])
+        model = Ridge(alpha=lambda_,fit_intercept=False)  # scipy has other naming convention
+        reg = model.fit(powers_of_x.T, ys)
 
-#         model = Ridge(alpha=lambda_,fit_intercept=False)  # scipy has other naming convention
-#         reg = model.fit(powers_of_x.T, ys)
+        models.append(reg)
 
-#         models.append(reg)
-
-#     return models
-
-
-# biases = []
-# variances = []
-
-# lambdas = [x for x in np.linspace(0, 2, 5)]
-# for lambda_ in lambdas:
-
-#     print(f"Training on lambde = {lambda_}")
-#     learning_algorithm = 5 # per problem statement
-#     learning_sets = make_learning_sets(NB_LEARNING_SETS)
-#     models = train_models_ridge(learning_sets, learning_algorithm, lambda_)
-#     bias = []
-#     variance = []
-#     for x0 in np.arange(0, 2, 0.01):
-#         bias.append(squared_bias_of_models_at_x(
-#             learning_algorithm, models, x0, bayes(x0)))
-#         variance.append(variance_of_models_at_x(
-#             learning_algorithm, models, x0, bayes(x0)))
-
-#     biases.append(bias)
-#     variances.append(variance)
+    return models
 
 
-# plt.figure(23)
-# for algo, variance in enumerate(variances):
-#     plt.scatter(np.arange(0, 2, 0.01), variance, marker='.', linewidths=0, label=f"m=5, lambda={lambdas[algo]}")
-# plt.ylim(0, 0.1)
-# plt.title("Variance")
-# plt.xlabel("x")
-# plt.legend()
-# plt.savefig("q2f_variance.pdf")
+biases = []
+variances = []
+
+lambdas = [x for x in np.linspace(0, 2, 5)]
+for lambda_ in lambdas:
+
+    print(f"Training on lambda = {lambda_}")
+    learning_algorithm = 5 # per problem statement
+    learning_sets = make_learning_sets(NB_LEARNING_SETS)
+    models = train_models_ridge(learning_sets, learning_algorithm, lambda_)
+    bias = []
+    variance = []
+    for x0 in X_RANGE:
+        bias.append(squared_bias_of_models_at_x(
+            learning_algorithm, models, x0, bayes(x0)))
+        variance.append(variance_of_models_at_x(
+            learning_algorithm, models, x0, bayes(x0)))
+
+    biases.append(bias)
+    variances.append(variance)
 
 
-# plt.figure(24)
-# for algo, bias in enumerate(biases):
-#     plt.scatter(np.arange(0, 2, 0.01), bias, marker='.', linewidths=0, label=f"m=5, lambda={lambdas[algo]:.1f}")
-# plt.ylim(0, 0.1)
-# plt.title("Bias")
-# plt.xlabel("x")
-# plt.legend()
-# plt.savefig("q2f_bias.pdf")
+plt.figure(23)
+for algo, variance in enumerate(variances):
+    plt.scatter(X_RANGE, variance, marker='.', linewidths=0, label=f"m=5, lambda={lambdas[algo]}")
+plt.ylim(0, 0.1)
+plt.title("Variance")
+plt.xlabel("x")
+plt.legend()
+plt.savefig("q2f_variance.pdf")
+
+
+plt.figure(24)
+for algo, bias in enumerate(biases):
+    plt.scatter(X_RANGE, bias, marker='.', linewidths=0, label=f"m=5, lambda={lambdas[algo]:.1f}")
+plt.ylim(0, 0.1)
+plt.title("Bias")
+plt.xlabel("x")
+plt.ylim(0, 0.02)
+plt.legend()
+plt.savefig("q2f_bias.pdf")
 
 plt.show()